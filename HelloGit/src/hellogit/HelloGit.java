9ba5db30ed026599c35f43ffd501e24907b7d04a
ropeSnake <ghastlyluke@gmail.com>
2018-09-20 09:54:11 -0500

    ligma

--- HelloGit/src/hellogit/HelloGit.java
+++ HelloGit/src/hellogit/HelloGit.java
@@ -4,6 +4,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Citrus Squad FTW");
+		System.out.println("what is this");
+		System.out.println("hello");
 	}
 
 }
